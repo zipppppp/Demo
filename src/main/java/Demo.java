3f6e0283959e4a8977445d6911acfb9556f922fd
tuya <819342837@qq.com>
2018-06-24 10:42:30 +0800

    test1

---
+++ src/main/java/Demo.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * show 方法的简述.
+ * ＜p＞show 方法的详细说明第一行＜br＞ * show 方法的详细说明第二行
+ */
+public class Demo {
+    public static void main(String[] args) {
+        List<String> a = new ArrayList<>();
+        a.add("2");
+        a.add("2");
+    }
+}
